ca239e5da5553ea6d4a651de42c8aab17c4162e5
Romanos Trechlis <r.trechlis@gmail.com>
2017-12-31 09:54:01 +0200

    change writer builder
    support database logging

--- writer/writer.go
+++ writer/writer.go
@@ -17,19 +17,27 @@ import (
 
 // RPCWriter implements Writer interface
 type RPCWriter struct {
-	conn     *grpc.ClientConn
-	path     string
-	filename string
+	target
+	conn *grpc.ClientConn
 }
 
 type builderImpl struct {
+	target
+	address string
+	port    int
+	cert    string
+	key     string
+	ca      string
+}
+
+type target struct {
+	isDir    bool
 	path     string
 	filename string
-	address  string
-	port     int
-	cert     string
-	key      string
-	ca       string
+
+	isDB     bool
+	dbServer string
+	dbName   string
 }
 
 // Builder interface holds the option methods
@@ -37,6 +45,7 @@ type builderImpl struct {
 type Builder interface {
 	WithPath(path string) Builder
 	WithFilename(filename string) Builder
+	WithDatabase(dbServer, dbName string) Builder
 	WithSecurity(cert, key, ca string) Builder
 	Build() (*RPCWriter, error)
 }
@@ -45,22 +54,35 @@ type Builder interface {
 // for the creation of RPCWriter when Build method gets called.
 func NewBuilder(address string, port int) Builder {
 	return builderImpl{
-		address:  address,
-		port:     port,
-		path:     "temp",
-		filename: "temporary",
+		address: address,
+		port:    port,
+		target: target{
+			isDir: false,
+			isDB:  false,
+		},
 	}
 }
 
 // WithFilename adds a non default filename parameter to builder
 func (b builderImpl) WithFilename(filename string) Builder {
 	b.filename = filename
+	b.isDir = true
 	return b
 }
 
 // WithPath adds a non default path parameter to builder
 func (b builderImpl) WithPath(path string) Builder {
 	b.path = path
+	b.isDir = true
+	return b
+}
+
+// WithDatabase adds database support
+func (b builderImpl) WithDatabase(dbServer, dbName string) Builder {
+	b.dbServer = dbServer
+	b.dbName = dbName
+	b.isDB = true
+	b.isDir = false
 	return b
 }
 
@@ -74,37 +96,28 @@ func (b builderImpl) WithSecurity(cert, key, ca string) Builder {
 
 // Build creates a new RPCWriter given the Builder parameters.
 func (b builderImpl) Build() (*RPCWriter, error) {
-	return New(b.path, b.filename, b.address, b.port, b.cert, b.key, b.ca)
+	return newRPCWriter(b)
 }
 
-// New creates an RPCWriter
-func New(path, filename, address string, port int, cert, key, ca string) (*RPCWriter, error) {
+// new creates an RPCWriter
+func newRPCWriter(b builderImpl) (*RPCWriter, error) {
 	s := scribe{
-		address: address,
-		port:    port,
-	}
-	config := gRPCConnectionConfig{
-		cert: cert,
-		key:  key,
-		ca:   ca,
+		address: b.address,
+		port:    b.port,
 	}
 
-	conn, err := createConnection(config, s)
+	conn, err := createConnection(b.cert, b.key, b.ca, s)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create connection to scribe: %v", err)
 	}
 
+	if b.isDB {
+		b.isDir = false
+	}
 	return &RPCWriter{
-		conn:     conn,
-		path:     path,
-		filename: filename,
+		conn:   conn,
+		target: b.target,
 	}, nil
-}
-
-type gRPCConnectionConfig struct {
-	cert string
-	key  string
-	ca   string
 }
 
 type scribe struct {
@@ -112,19 +125,19 @@ type scribe struct {
 	port    int
 }
 
-func createConnection(sec gRPCConnectionConfig, sc scribe) (*grpc.ClientConn, error) {
+func createConnection(cert, key, ca string, sc scribe) (*grpc.ClientConn, error) {
 	var conn *grpc.ClientConn
 	server := fmt.Sprintf("%s:%d", sc.address, sc.port)
-	if sec.cert != "" && sec.key != "" && sec.ca != "" {
+	if cert != "" && key != "" && ca != "" {
 		// Load the client certificates from disk
-		certificate, err := tls.LoadX509KeyPair(sec.cert, sec.key)
+		certificate, err := tls.LoadX509KeyPair(cert, key)
 		if err != nil {
 			return nil, fmt.Errorf("could not load client key pair: %s", err)
 		}
 
 		// Create a certificate pool from the certificate authority
 		certPool := x509.NewCertPool()
-		ca, err := ioutil.ReadFile(sec.ca)
+		ca, err := ioutil.ReadFile(ca)
 		if err != nil {
 			return nil, fmt.Errorf("could not read ca certificate: %s", err)
 		}
@@ -182,7 +195,24 @@ func (w *RPCWriter) Write(p []byte) (n int, err error) {
 
 // NewLogger creates a *Logger with default prefix and flags and a new RPCWriter
 func NewLogger(path, filename, address string, port int, cert, key, ca string) (*log.Logger, error) {
-	w, err := New(path, filename, address, port, cert, key, ca)
+	w, err := NewBuilder(address, port).
+		WithFilename(filename).
+		WithPath(path).
+		WithSecurity(cert, key, ca).
+		Build()
+	if err != nil {
+		return nil, err
+	}
+	flag := log.Ldate | log.Ltime | log.Lshortfile
+	return log.New(w, "", flag), nil
+}
+
+// NewLogger creates a *Logger with default prefix and flags and a new RPCWriter
+func NewDBLogger(dbServer, dbName, address string, port int, cert, key, ca string) (*log.Logger, error) {
+	w, err := NewBuilder(address, port).
+		WithDatabase(dbServer, dbName).
+		WithSecurity(cert, key, ca).
+		Build()
 	if err != nil {
 		return nil, err
 	}
